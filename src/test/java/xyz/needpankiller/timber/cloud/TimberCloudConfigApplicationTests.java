1af7231993e24f7d91f396d28db7ac6d43384fdb
needpainkiller <needpainkiller6512@gmail.com>
2024-04-21 21:13:32 +0900

    Add : Configuration Classes

---
+++ src/test/java/xyz/needpankiller/timber/cloud/TimberCloudConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.needpankiller.timber.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimberCloudConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
